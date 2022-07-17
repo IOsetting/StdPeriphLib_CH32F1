70b352eff5d39f3279b00594e0e0b5da9df4ff06
IOsetting <iosetting@outlook.com>
2022-07-17 09:30:02 +0800

    style: code format

--- Examples/I2C/I2C_7bit_Mode/Main.c
+++ Examples/I2C/I2C_7bit_Mode/Main.c
@@ -20,10 +20,10 @@
 
 /* I2C Mode Definition */
 #define HOST_MODE   0
-#define SLAVE_MODE   1
+#define SLAVE_MODE  1
 
 /* I2C Communication Mode Selection */
-#define I2C_MODE   HOST_MODE
+#define I2C_MODE HOST_MODE
 //#define I2C_MODE   SLAVE_MODE
 
 /* Global define */
@@ -31,8 +31,8 @@
 #define RXAdderss   0x02
 #define TxAdderss   0x02
 
-/* Global Variable */ 
-u8 TxData[Size] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
+/* Global Variable */
+u8 TxData[Size] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
 u8 RxData[Size];
 
 /*******************************************************************************
@@ -66,7 +66,7 @@ void IIC_Init( u32 bound, u16 address )
     I2C_InitTSturcture.I2C_Ack = I2C_Ack_Enable;
     I2C_InitTSturcture.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
     I2C_Init(I2C1, &I2C_InitTSturcture);
-    I2C_Cmd( I2C1, ENABLE );
+    I2C_Cmd(I2C1, ENABLE);
 
 #if (I2C_MODE == HOST_MODE)
     I2C_AcknowledgeConfig( I2C1, ENABLE );
@@ -85,52 +85,52 @@ int main(void)
 
     Delay_Init();
     USART_Printf_Init(460800);
-    printf("SystemClk:%d\r\n",SystemCoreClock);
+    printf("SystemClk:%ld\r\n",SystemCoreClock);
 
 #if (I2C_MODE == HOST_MODE)
     printf("IIC Host mode\r\n");
-    IIC_Init( 80000, TxAdderss);
-    
-    while( I2C_GetFlagStatus( I2C1, I2C_FLAG_BUSY ) != RESET );
-    
-    I2C_GenerateSTART( I2C1, ENABLE );
-    
-    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_MODE_SELECT ) );
-    I2C_Send7bitAddress( I2C1, 0x02, I2C_Direction_Transmitter );
-    
-    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) );
+    IIC_Init(80000, TxAdderss);
 
-    while( i<6 )
+    while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY) != RESET);
+
+    I2C_GenerateSTART(I2C1, ENABLE);
+
+    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
+    I2C_Send7bitAddress(I2C1, 0x02, I2C_Direction_Transmitter);
+
+    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+
+    while (i < 6)
     {
-        if( I2C_GetFlagStatus( I2C1, I2C_FLAG_TXE ) !=  RESET )
+        if (I2C_GetFlagStatus(I2C1, I2C_FLAG_TXE) != RESET)
         {
-            I2C_SendData( I2C1, TxData[i] );
+            I2C_SendData(I2C1, TxData[i]);
             i++;
         }
     }
 
-    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED ) );
-    I2C_GenerateSTOP( I2C1, ENABLE );
+    while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+    I2C_GenerateSTOP(I2C1, ENABLE);
 
 #elif (I2C_MODE == SLAVE_MODE)
     printf("IIC Slave mode\r\n");
-    IIC_Init( 80000, RXAdderss);
-    
-    while( !I2C_CheckEvent( I2C1, I2C_EVENT_SLAVE_RECEIVER_ADDRESS_MATCHED ) );
+    IIC_Init(80000, RXAdderss);
 
-    while( i<6 )
+    while (!I2C_CheckEvent(I2C1, I2C_EVENT_SLAVE_RECEIVER_ADDRESS_MATCHED));
+
+    while (i < 6)
     {
-        if( I2C_GetFlagStatus( I2C1, I2C_FLAG_RXNE ) !=  RESET )
+        if (I2C_GetFlagStatus(I2C1, I2C_FLAG_RXNE) != RESET)
         {
-            RxData[i] = I2C_ReceiveData( I2C1 );
+            RxData[i] = I2C_ReceiveData(I2C1);
             i++;
         }
     }
 
-    printf( "RxData:\r\n" );
-    for( i=0; i<6; i++ )
+    printf("RxData:\r\n");
+    for (i = 0; i < 6; i++)
     {
-        printf( "%02x\r\n", RxData[i] );
+        printf("%02x\r\n", RxData[i]);
     }
 
 #endif
